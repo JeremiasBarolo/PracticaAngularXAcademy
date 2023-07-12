d45e4a3b0578d83377d53d72a91119793dcbaa51
Jeremias Barolo <barolojeremias@gmail.com>
2023-07-12 05:59:38 -0300

    Consigna B + Opcional Terminada
    Creacion de type de Arreglo y mostrar en el html con el ngFor.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+
+// Definicion del Arreglo
+type Arreglo = {
+  nombre: string;
+  costos: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'Ejercicio Practica Angular';
+
+  personajes: Arreglo[] = []
+
+  constructor(){
+    // Rellenando el arreglo
+    this.personajes.push({nombre: 'Batman', costos: '100'}) 
+    this.personajes.push({nombre: 'Superman', costos: '200'})
+  }
+
+  
+
+
 }
